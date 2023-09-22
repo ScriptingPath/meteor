51f7ef0d95c4c45b61ef4953b37c4eba4442d61e
ScriptingPath <74320985+ScriptingPath@users.noreply.github.com>
2023-09-22 12:22:02 +0400

    Update api.lua

--- api.lua
+++ api.lua
@@ -2,11 +2,41 @@
 
 local api = {}
 
+local RunService = game:GetService("RunService")
+
 local void = function() return 0 end
 local setidentity = syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or void
 local getidentity = syn and syn.get_thread_identity or get_thread_identity or getidentity or getthreadidentity or void
 
-local function api.displayPopup(title, text, callback, buttonName)
+function api:UnbindFromRenderStep(name)
+    if name then
+        return RunService:UnbindFromRenderStep(tostring(name))
+    end
+
+    warn("UnbindFromRenderStep name is nil (meteorapi)")
+end
+
+
+function api:BindToRenderStep(name, priority, func, unbind)
+    if unbind == nil then unbind = true end
+
+    if not func then
+        return warn("BindToRenderStep func is nil (meteorapi)")
+    end
+
+    if unbind and name then
+        self:UnbindFromRenderStep(name)
+    end
+
+    if not name or priority then
+        return RunService.RenderStepped:Connect(func)
+    end
+
+    return RunService:BindToRenderStep(tostring(name), tonumber(priority), func)
+end
+
+
+function api:DisplayPopup(title, text, callback, buttonName)
 	local oldidentity = getidentity()
 	setidentity(8)
 	local ErrorPrompt = getrenv().require(game:GetService("CoreGui").RobloxGui.Modules.ErrorPrompt)
